b955ea38500597752d051793430a59944304c0b3
sersek <93370936+sersek@users.noreply.github.com>
2021-12-17 09:29:22 +0100

    Add files via upload

---
+++ exercici10.py
@@ -0,0 +1,18 @@
+""""
+Supos que el que pot pasar es que el programa dirà si les afirmacions són vertaderes o falses
+
+x = 5
+y = 10
+z = 10
+print(x < y)
+print(y == z)
+print(x == 5)
+print(not x == 5)
+print(x != 5)
+print(not x != 5)
+print(x == "5")
+print(5 == x + 0.00000000001)
+print(x == 5 and y == 10)
+print(x == 5 and y == 5)
+print(x == 5 or y == 5)
+print(not x == 5)
