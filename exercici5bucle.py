f66021043fc334871a44eed87821e7a8ee0d89b0
sersek <93370936+sersek@users.noreply.github.com>
2022-01-10 10:17:33 +0100

    Add files via upload

---
+++ exercici5bucle.py
@@ -0,0 +1,7 @@
+total = 0
+print("Este programa recibe tres números y devuelve la suma.")
+
+for i in range (3):
+    x = int(input("Introduce un número: "))
+    total = total + x
+print("El total es:", total)
